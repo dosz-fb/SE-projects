a8a75a96f21a713cdca065f2cbffbf16f60b1641
doug <54515194+dosz-fb@users.noreply.github.com>
2020-05-27 15:12:24 -0400

    Update spend_limit_cap.sh

--- spend_limit/spend_limit_cap.sh
+++ spend_limit/spend_limit_cap.sh
@@ -21,16 +21,22 @@ do
     fi
 
     echo "action = set spend cap for account ${AD_ACCOUNT_ID} to ${SPEND_CAP}"
+    
+    # check the existing spending limit
     preChange="$(curl -X GET -s -G \
                     --data access_token=${ACCESS_TOKEN} \
                     --data fields=spend_cap,amount_spent \
                     https://graph.facebook.com/v7.0/act_${AD_ACCOUNT_ID})"
     echo "pre = $preChange"
+    
+    # change the spend limit value
     doChange="$(curl -X POST -s \
                     --data spend_cap=${SPEND_CAP} \
                     --data access_token=${ACCESS_TOKEN} \
                     https://graph.facebook.com/v6.0/act_${AD_ACCOUNT_ID})"
     echo "do = $doChange"
+    
+    # check the new spending limit
     postChange="$(curl -X GET -s -G \
                     --data access_token=${ACCESS_TOKEN} \
                     --data fields=spend_cap,amount_spent \
