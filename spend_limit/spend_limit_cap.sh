026d7750d89d6092eb4e5f29f994418b0db6b6d4
doug <54515194+dosz-fb@users.noreply.github.com>
2020-05-27 15:05:05 -0400

    Update spend_limit_cap.sh

--- spend_limit/spend_limit_cap.sh
+++ spend_limit/spend_limit_cap.sh
@@ -2,6 +2,62 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+AD_ACCOUNTS_CSV_FILE=$1
+ACCESS_TOKEN_FILE=$2
+
+# Read the access token file
+ACCESS_TOKEN=$(cat ${ACCESS_TOKEN_FILE})
+
+while IFS="," read -r AD_ACCOUNT_ID SPEND_CAP
+do
+    if [ -z "${AD_ACCOUNT_ID}" ]; then
+        echo "action = account is blank, skipping"
+        continue
+    fi
+
+    if [ -z "${SPEND_CAP}" ]; then
+        echo "action = spend cap for account ${AD_ACCOUNT_ID} is blank, skipping"
+        continue
+    fi
+
+    echo "action = set spend cap for account ${AD_ACCOUNT_ID} to ${SPEND_CAP}"
+    preChange="$(curl -X GET -s -G \
+                    --data access_token=${ACCESS_TOKEN} \
+                    --data fields=spend_cap,amount_spent \
+                    https://graph.facebook.com/v7.0/act_${AD_ACCOUNT_ID}?access_token=${ACCESS_TOKEN}&fields=spend_cap%2Camount_spent)"
+    echo "pre = $preChange"
+    doChange="$(curl -X POST -s \
+                    --data spend_cap=${SPEND_CAP} \
+                    --data access_token=${ACCESS_TOKEN} \
+                    https://graph.facebook.com/v6.0/act_${AD_ACCOUNT_ID})"
+    echo "do = $doChange"
+    postChange="$(curl -X GET -s -G \
+                    --data access_token=${ACCESS_TOKEN} \
+                    --data fields=spend_cap,amount_spent \
+                    https://graph.facebook.com/v7.0/act_${AD_ACCOUNT_ID})"
+    echo "post = $postChange"
+done < ${AD_ACCOUNTS_CSV_FILE}
+
+echo "done"
+
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+AD_ACCOUNTS_FILE=$1
+ACCESS_TOKEN_FILE=$2
+
+ACCESS_TOKEN=$(cat ${ACCESS_TOKEN_FILE})
+for AD_ACCOUNT_ID in $(cat ${AD_ACCOUNTS_FILE}); do
+done
+
+echo "done"
+
+
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 API_VERSION="v7.0"
 AD_ACCOUNTS_CSV_FILE=$1
 ACCESS_TOKEN_FILE=$2
@@ -11,23 +67,23 @@ ACCESS_TOKEN=$(cat ${ACCESS_TOKEN_FILE})
 
 while IFS="," read -r AD_ACCOUNT_ID SPEND_CAP
 do
-    echo "action = resetting counter for account ${AD_ACCOUNT_ID}"
+    echo "action = set spend cap for account ${AD_ACCOUNT_ID} to ${SPEND_CAP}"
 
-    # check the existing spending limit
+    # check the current value
     preChange="$(curl -X GET -s -G \
                     --data access_token=${ACCESS_TOKEN} \
                     --data fields=spend_cap,amount_spent \
-                    https://graph.facebook.com/${API_VERSION}/act_${AD_ACCOUNT_ID})"
+                    https://graph.facebook.com/${API_VERSION}/act_${AD_ACCOUNT_ID}?access_token=${ACCESS_TOKEN}&fields=spend_cap%2Camount_spent)"
     echo "pre = $preChange"
 
-    # reset the spent counter
+    # set the spend cap value
     doChange="$(curl -X POST -s \
-                    --data spend_cap_action=reset \
+                    --data spend_cap=${SPEND_CAP} \
                     --data access_token=${ACCESS_TOKEN} \
                     https://graph.facebook.com/${API_VERSION}/act_${AD_ACCOUNT_ID})"
     echo "do = $doChange"
 
-    # check the new spending limit
+    # check the changed value
     postChange="$(curl -X GET -s -G \
                     --data access_token=${ACCESS_TOKEN} \
                     --data fields=spend_cap,amount_spent \
